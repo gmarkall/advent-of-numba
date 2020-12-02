1472aa62762a688784987b56db89873cdd2593df
Graham Markall <gmarkall@nvidia.com>
2020-12-02 13:00:26 +0000

    Day 2 part 1 solution

---
+++ day02/solution.py
@@ -0,0 +1,63 @@
+from math import ceil
+from numba import cuda
+import numpy as np
+
+
+def parse(line):
+    # Maybe a regex would be better, but I am rubbish at those.
+    lower = int(line.split('-')[0])
+    upper = int(line.split('-')[1].split()[0])
+    letter = ord(line.split()[1][0])
+    password = [ord(x) for x in line.split()[2]]
+    length = len(password)
+    return lower, upper, letter, length, password
+
+
+lowers, uppers, letters, lengths, passwords = [], [], [], [], []
+with open('input') as f:
+    for line in f.readlines():
+        lower, upper, letter, length, password = parse(line)
+        lowers.append(lower)
+        uppers.append(upper)
+        letters.append(letter)
+        lengths.append(length)
+        passwords.append(password)
+
+max_length = max(lengths)
+for i in range(len(passwords)):
+    if len(passwords[i]) < max_length:
+        passwords[i].extend([0] * (max_length - len(passwords[i])))
+
+dtype = [
+    ('lower', '<i4'),
+    ('upper', '<i4'),
+    ('letter', '<i4'),
+    ('length', '<i4'),
+    ('password', '<i4', max(lengths))
+]
+
+entries = np.array(list(zip(lowers, uppers, letters, lengths, passwords)),
+                   dtype=dtype)
+
+
+@cuda.jit
+def count_valid_passwords(entries, answer):
+    i = cuda.grid(1)
+    entry = entries[i]
+    if i < len(entries):
+        count = 0
+        letter = entry['letter']
+        password = entry['password']
+        for p in range(entry['length']):
+            if password[p] == letter:
+                count += 1
+        if count >= entry['lower'] and count <= entry['upper']:
+            cuda.atomic.inc(answer, 0, len(entries))
+
+
+n_threads = 256
+n_blocks = ceil(len(entries) / n_threads)
+
+answer = np.zeros(1, dtype=np.uint32)
+count_valid_passwords[n_blocks, n_threads](entries, answer)
+print(f"The answer to part 1 is {answer[0]}")
