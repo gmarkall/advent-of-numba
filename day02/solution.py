eb7b057a2dbb11f2a81e614c96be556d291d286e
Graham Markall <gmarkall@nvidia.com>
2020-12-02 13:10:31 +0000

    Add day 2 solutions

--- day02/solution.py
+++ day02/solution.py
@@ -43,8 +43,8 @@ entries = np.array(list(zip(lowers, uppers, letters, lengths, passwords)),
 @cuda.jit
 def count_valid_passwords(entries, answer):
     i = cuda.grid(1)
-    entry = entries[i]
     if i < len(entries):
+        entry = entries[i]
         count = 0
         letter = entry['letter']
         password = entry['password']
@@ -61,3 +61,21 @@ n_blocks = ceil(len(entries) / n_threads)
 answer = np.zeros(1, dtype=np.uint32)
 count_valid_passwords[n_blocks, n_threads](entries, answer)
 print(f"The answer to part 1 is {answer[0]}")
+
+
+@cuda.jit
+def count_valid_new_policy(entries, answer):
+    i = cuda.grid(1)
+    if i < len(entries):
+        entry = entries[i]
+        p1 = entry['password'][entry['lower'] - 1]
+        p2 = entry['password'][entry['upper'] - 1]
+        letter = entry['letter']
+        matches = int(p1 == letter) + int(p2 == letter)
+        if matches == 1:
+            cuda.atomic.inc(answer, 0, len(entries))
+
+
+answer = np.zeros(1, dtype=np.uint32)
+count_valid_new_policy[n_blocks, n_threads](entries, answer)
+print(f"The answer to part 2 is {answer[0]}")
