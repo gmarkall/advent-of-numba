ac0f45abcd8928eed5fba7f91fa7dce501c8a739
Graham Markall <gmarkall@nvidia.com>
2020-12-08 21:14:06 +0000

    Day 8 Part 2 solution using CUDA

--- day08/solution.py
+++ day08/solution.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numba import njit
+from math import ceil
+from numba import cuda, njit, types
 
 with open('input') as f:
     lines = f.readlines()
@@ -26,10 +27,11 @@ def assemble(lines):
 
 
 binary = assemble(lines)
+n_instrs = len(lines)
 
 
 @njit
-def execute(binary, visited):
+def execute(binary, visited, terminated, mutation):
     pc = 0
     accumulator = 0
     while not visited[pc]:
@@ -38,6 +40,14 @@ def execute(binary, visited):
 
         # Extract opcode
         opcode = (instr & 0xC000) >> 14
+
+        # Mutate the opcode if required
+        if pc == mutation:
+            #print(f"Mutating {pc}")
+            if opcode == NOP:
+                opcode = JMP
+            elif opcode == JMP:
+                opcode = NOP
 
         # Decode signed 14-bit operand
         operand = (instr & 0x3FFF)
@@ -52,9 +62,42 @@ def execute(binary, visited):
             pc += 1
         elif opcode == JMP:
             pc += operand
+
+        if pc == n_instrs:
+            terminated[0] = True
+            return accumulator
+
     return accumulator
 
 
-visited = np.zeros(len(lines), dtype=np.bool_)
-result = execute(binary, visited)
+visited = np.zeros(n_instrs, dtype=np.bool_)
+
+# Don't mutate any instruction
+mutation = -1
+terminated = np.zeros(1, dtype=np.bool_)
+result = execute(binary, visited, terminated, mutation)
 print(f'The answer to part 1 is {result}')
+
+
+@cuda.jit
+def brute_force_mutations(binary, visited, answer):
+    i = cuda.grid(1)
+    if i >= n_instrs:
+        return
+
+    terminated = cuda.local.array(1, dtype=types.boolean)
+    result = execute(binary, visited[i], terminated, i)
+
+    if terminated[0]:
+        #print(f"Thread {i} finds answer {result}")
+        answer[0] = result
+
+
+visited = np.zeros((n_instrs, n_instrs), dtype=np.bool_)
+answer = np.zeros(1, dtype=np.uint32)
+
+n_threads = 256
+n_blocks = ceil(n_instrs / n_threads)
+
+brute_force_mutations[n_blocks, n_threads](binary, visited, answer)
+print(f'The answer to part 2 is {answer[0]}')
