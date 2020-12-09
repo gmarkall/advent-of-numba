e09026d1a4c2aaf4e3c235c92923d6be5752dbea
Graham Markall <gmarkall@nvidia.com>
2020-12-09 17:02:29 +0000

    Rewrite day 9 part 1 using CUDA

--- day09/solution.py
+++ day09/solution.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-import itertools
+from numba import cuda
+from math import ceil
+import numpy as np
+
 
 USE_EXAMPLE = False
 
@@ -12,42 +15,51 @@ else:
     preamble_length = 25
 
 with open(filename) as f:
-    seq = [int(x.strip()) for x in f.readlines()]
+    seq = np.asarray([int(x.strip()) for x in f.readlines()], dtype=np.uint32)
 
 
-def find_impostor(seq):
-    lower = 0
-    upper = preamble_length
-    while upper < len(seq):
-        current = seq[upper]
-        window = seq[lower:upper]
+@cuda.jit
+def find_impostor(seq, answer):
+    i = cuda.grid(1)
+    lower = i
+    upper = lower + preamble_length
 
-        found = False
-        for x, y in itertools.product(window, window):
+    if upper >= len(seq):
+        return
+
+    current = seq[upper]
+
+    found = False
+    for xi in range(lower, upper):
+        for yi in range(lower, upper):
+            x = seq[xi]
+            y = seq[yi]
+
             if x == y:
                 continue
-            if (x + y) == current:
+            if x + y == current:
                 found = True
-                lower += 1
-                upper += 1
-                break
 
-        if not found:
-            return current
+    if not found:
+        cuda.atomic.min(answer, 0, upper)
 
 
-impostor = find_impostor(seq)
+n_threads = 256
+n_blocks = ceil(len(seq) / n_threads)
+
+impostor_idx = np.asarray([len(seq) + 1], dtype=np.uint32)
+find_impostor[n_blocks, n_threads](seq, impostor_idx)
+impostor = seq[impostor_idx[0]]
 print(f"The answer to part 1 is {impostor}")
 
 
-def find_contiguous(impostor, seq):
-    loc = seq.index(impostor)
-    for i in range(loc):
-        for j in range(loc - i):
+def find_contiguous(impostor_idx, seq):
+    for i in range(impostor_idx):
+        for j in range(impostor_idx - i):
             candidate = seq[i:i+j]
             if sum(candidate) == impostor:
                 return (min(candidate) + max(candidate))
 
 
-answer = find_contiguous(impostor, seq)
+answer = find_contiguous(impostor_idx[0], seq)
 print(f"The answer to part 2 is {answer}")
